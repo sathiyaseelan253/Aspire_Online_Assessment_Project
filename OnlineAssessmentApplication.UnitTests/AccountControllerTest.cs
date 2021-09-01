fb93accb5f4a7e2af8af73bae42c8a0d7ab4d56a
Sathiyaseelan Ramasamy <46224864+sathiyaseelan253@users.noreply.github.com>
2021-09-01 10:23:59 +0530

    Unit testing files

---
+++ OnlineAssessmentApplication.UnitTests/AccountControllerTest.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineAssessmentApplication.Controllers;
+using OnlineAssessmentApplication.DomainModel;
+using OnlineAssessmentApplication.Repository;
+using OnlineAssessmentApplication.ViewModel;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace OnlineAssessmentApplication.UnitTests
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        private readonly UserRepository userRepository;
+        public AccountControllerTest()
+        {
+            this.userRepository = new UserRepository();
+        }
+        [TestMethod]
+        public void LoginTest()
+        {
+            //Arrange
+            User userDetails = null;
+
+            //Act
+            userDetails=userRepository.ValidateUser(new UserViewModel() { EmailID = "student@gmail.com", Password = "123" }).FirstOrDefault();
+
+            //Assert
+            Assert.IsNotNull(userDetails);
+        }
+        [TestMethod]
+        public void FindRole()
+        {
+            string expectedRole = "Student";
+            //Arrange
+            string role = string.Empty;
+            //Act
+            role= userRepository.FindRole(new UserViewModel() { EmailID = "student@gmail.com", Password = "123" });
+            //Assert
+            Assert.AreEqual(role, expectedRole);
+        }
+        [TestMethod]
+        public void CheckIsIsPresentOrNot()
+        {
+            //Arrange
+            int id = 2;
+           
+            //Act
+            User fetchedUserDetails= userRepository.GetUserByUserId(id);
+
+            //Assert
+            Assert.IsNotNull(fetchedUserDetails);
+        }
+        [TestMethod]
+        public void CheckUpdateUserProfile()
+        {
+            //Arrange
+            int id = 2;
+            bool statusForUserProfileUpdate = false;
+
+            //Act
+            User fetchedUserDetails = userRepository.GetUserByUserId(id);
+            if (fetchedUserDetails != null)
+            {
+                userRepository.UpateUserDetails(new User() { UserId = 2, Name = "Prasanth kumar", PhoneNumber = 123456 });
+                statusForUserProfileUpdate = true;
+
+            }
+
+
+            //Assert
+            Assert.IsTrue(statusForUserProfileUpdate);
+
+        }
+        [TestMethod]
+        public void CheckUpdatePassword()
+        {
+            int id = 2;
+            bool statusForPasswordUpdate = false;
+
+            //Act
+            User fetchedUserDetails = userRepository.GetUserByUserId(id);
+            if (fetchedUserDetails != null)
+            {
+                userRepository.UpdateUserPasswordDetails(new User() { Password = "123", UserId = 2 });
+                statusForPasswordUpdate = true;
+            }
+
+            //Assert
+            Assert.IsTrue(statusForPasswordUpdate);
+        }
+        [TestMethod]
+        public void CheckView()
+        {
+            //Arrange
+            AccountController accountController = new AccountController();
+
+            //Act
+            ViewResult result= accountController.Login() as ViewResult;
+
+            //Assert
+            Assert.AreEqual("Login", result.ViewName);
+        }
+    }
+}
---
+++ OnlineAssessmentApplication.UnitTests/AssessmentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using OnlineAssessmentApplication.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAssessmentApplication.UnitTests
+{
+    public class AssessmentDbContext : DbContext
+    {
+        public AssessmentDbContext() : base("connectionStringToDB")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
